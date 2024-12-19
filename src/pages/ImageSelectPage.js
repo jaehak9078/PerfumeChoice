f1dcae19eae6eef60ca8b38b2ba4d65dd33c87ae
sgt7678 <96823293+humanmaker@users.noreply.github.com>
2024-12-19 16:11:14 +0900

    계절 이미지 선택 페이지 수정

--- src/pages/ImageSelectPage.js
+++ src/pages/ImageSelectPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
 const ImageSelectPage = () => {
+  const handleNavigation = (season) => {
+    // 예: 페이지 이동 로직
+    window.location.href = `/${season}`;
+  };
+
   return (
     <div className="image-container">
       <img src="/Seasons.jpg" alt="seasons" className="responsive-image2" />
+      <button className="season-button spring" onClick={() => handleNavigation('spring')}>Spring</button>
+      <button className="season-button summer" onClick={() => handleNavigation('summer')}>Summer</button>
+      <button className="season-button autumn" onClick={() => handleNavigation('autumn')}>Autumn</button>
+      <button className="season-button winter" onClick={() => handleNavigation('winter')}>Winter</button>
     </div>
   );
 };
