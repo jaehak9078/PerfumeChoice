974e49e54782a32898719dd771cde538d3cee8d9
sgt7678 <96823293+humanmaker@users.noreply.github.com>
2024-12-19 17:33:30 +0900

    계절 이미지 선택 페이지 수정

--- src/pages/ImageSelectPage.js
+++ src/pages/ImageSelectPage.js
@@ -8,10 +8,10 @@ const ImageSelectPage = () => {
   return (
     <div className="image-container">
       <img src="/Seasons.jpg" alt="seasons" className="responsive-image2" />
-      <button className="season-button spring" onClick={() => handleNavigation('spring')}>Spring</button>
-      <button className="season-button summer" onClick={() => handleNavigation('summer')}>Summer</button>
-      <button className="season-button autumn" onClick={() => handleNavigation('autumn')}>Autumn</button>
-      <button className="season-button winter" onClick={() => handleNavigation('winter')}>Winter</button>
+      <button className="season-button spring" onClick={() => handleNavigation('spring')}>따뜻하고 포근한 이미지</button>
+      <button className="season-button summer" onClick={() => handleNavigation('summer')}>뜨겁고 에너지가 넘치는 이미지</button>
+      <button className="season-button autumn" onClick={() => handleNavigation('autumn')}>선선하고 낭만적인 이미지</button>
+      <button className="season-button winter" onClick={() => handleNavigation('winter')}>차갑고 고요한 이미지</button>
     </div>
   );
 };
