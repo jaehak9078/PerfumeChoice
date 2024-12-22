8a8ac487b9e10f2fa841170bbcc41c71bf56df6d
sgt7678 <96823293+humanmaker@users.noreply.github.com>
2024-12-23 01:30:55 +0900

    계절 이미지 선택 페이지 수정

--- src/pages/ImageSelectPage.js
+++ src/pages/ImageSelectPage.js
@@ -6,12 +6,34 @@ const ImageSelectPage = () => {
   };
 
   return (
-    <div className="image-container">
-      <img src="/Seasons.jpg" alt="seasons" className="responsive-image2" />
-      <button className="season-button spring" onClick={() => handleNavigation('spring')}>따뜻하고 포근한 이미지</button>
-      <button className="season-button summer" onClick={() => handleNavigation('summer')}>뜨겁고 에너지가 넘치는 이미지</button>
-      <button className="season-button autumn" onClick={() => handleNavigation('autumn')}>선선하고 낭만적인 이미지</button>
-      <button className="season-button winter" onClick={() => handleNavigation('winter')}>차갑고 고요한 이미지</button>
+    <div className="season-container">
+      <div className="season">
+        <img src="/Spring.png" alt="Spring" />
+        <button className="season-button" onClick={() => handleNavigation('spring')}>
+        따뜻하고 포근한 이미지
+        </button>
+      </div>
+      
+      <div className="season">
+        <img src="/Summer.png" alt="Summer" />
+        <button className="season-button" onClick={() => handleNavigation('summer')}>
+        뜨겁고 에너지가 넘치는 이미지
+        </button>
+      </div>
+      
+      <div className="season">
+        <img src="/Autumn.png" alt="Autumn" />
+        <button className="season-button" onClick={() => handleNavigation('autumn')}>
+        선선하고 낭만적인 이미지
+        </button>
+      </div>
+      
+      <div className="season">
+        <img src="/Winter.png" alt="Winter" />
+        <button className="season-button" onClick={() => handleNavigation('winter')}>
+        차갑고 고요한 이미지
+        </button>
+      </div>
     </div>
   );
 };
